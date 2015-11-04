e3189b3a1867adf0c4e7748d7a9c9fe6e7148e97
Grigory Ostapenko <gmostapgv@gmail.com>
2015-11-04 18:00:36 +0100

    without modernzr

--- js/home.js
+++ js/home.js
@@ -5,18 +5,9 @@ mainApp.fn = (function($){
 	return {
 
 		toggleMenu: function($menu) {
-			var isVisible = $menu.css("opacity") != "0";
-			if(!isVisible) {
-				$menu.css("display", "block");
-			}
-			
-			$menu.animate({
-				opacity: isVisible ? 0 : 1,
-			}, 1000, function() {
-				if(isVisible){
-					$menu.css("display", "none");
-				}
-			});
+			// if (!$menu.is(':animated')) {
+				$menu.stop().fadeToggle( "slow", "linear" );
+			// }
 		}
 
 	};
