7e209053e384c71a7f34f92a2eb3ba291a9ed80a
Grigory Ostapenko <gmostapgv@gmail.com>
2015-11-18 10:53:52 +0100

    fixed issues descrybed 09.11.15

--- js/home.js
+++ js/home.js
@@ -1,56 +1,50 @@
-"use strict";
-var mainApp =  mainApp || {};
-
-mainApp.fn = (function($){
-	return {
-
-		toggleMenu: function($menu) {
-			// if (!$menu.is(':animated')) {
-			$menu.stop().fadeToggle( "slow", "linear" );
-			// }
-		}
-
-	};
+var mainApp = (function($){
+  "use strict";
+  return {
+    toggleMenu: function($menu) {
+      $menu.stop().fadeToggle( "slow", "linear" );
+    }
+  };
 }(jQuery));
 
 
 $(document).ready(function() {
-	// Menu click event
-	$(".sprite-menu, #main-menu li").on("click", function(){
-		mainApp.fn.toggleMenu($('#main-menu'));
-	});
+  // Menu click event
+  $(".sprite-menu").on("click", function(){
+    mainApp.toggleMenu($('#main-menu'));
+  });
 
-	// Slider initialization
-	$('.slider').bxSlider({
-		slideWidth: 80,
-		minSlides: 1,
-		maxSlides: 3,
-		slideMargin: 10
-	});
+  // Slider initialization
+  $('.slider').bxSlider({
+    slideWidth: 80,
+    minSlides: 1,
+    maxSlides: 3,
+    slideMargin: 10
+  });
 
-	// initializing a colorbox
-	$('a.news-img').colorbox({ 
-		opacity: 0.5 , 
-		rel: 'group1', 
-		open: false, //'true' to start immediately
-		width: 250
-	});
+  // initializing a colorbox
+  $('a.news-img').colorbox({ 
+    opacity: 0.5 , 
+    rel: 'group1', 
+    open: false, //'true' to start immediately
+    width: 250
+  });
 
-	// colorbox events
-	$(document).bind('cbox_complete', function() {
-		var $element = $.colorbox.element();
+  // colorbox events
+  $(document).bind('cbox_complete', function() {
+    var $element = $.colorbox.element();
 
-		if(!$element.hasClass('last')) {
-			$(this).data({'timeout-id': setTimeout($.colorbox.next, 2000)});
-		}else {
-			setTimeout($.colorbox.close, 2000);
-			$(this).unbind('cbox_complete');
-		}
-	});
+    if(!$element.hasClass('last')) {
+      $(this).data({'timeout-id': setTimeout($.colorbox.next, 2000)});
+    }else {
+      setTimeout($.colorbox.close, 2000);
+      $(this).unbind('cbox_complete');
+    }
+  });
 
-	$(document).bind('cbox_closed', function() {
-		$(this).unbind('cbox_complete');
-		clearTimeout($(this).data()['timeout-id']);
-	});
+  $(document).bind('cbox_closed', function() {
+    $(this).unbind('cbox_complete');
+    clearTimeout($(this).data()['timeout-id']);
+  });
 
 });
